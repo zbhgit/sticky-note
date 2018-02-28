99fa397f983b99d8bfeae407bf27d0c312c5518a
zbhgit <zbh_wj@163.com>
2018-02-28 17:23:37 +0800

    change callback ip

--- routes/auth.js
+++ routes/auth.js
@@ -14,11 +14,10 @@ passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
-
 passport.use(new GitHubStrategy({
   clientID: '25fb60fefe06bf27cb4c',
   clientSecret: 'feeefeea1f18d87b098680c5deb3d17580563528',
-  callbackURL: "http://101.236.23.219:3000//auth/github/callback"
+  callbackURL: "http://18.222.33.142:3000//auth/github/callback"
 },
   (accessToken, refreshToken, profile, done) => {
     // User.findOrCreate({ githubId: profile.id }, function (err, user) {
